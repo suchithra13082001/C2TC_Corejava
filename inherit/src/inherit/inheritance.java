0c5b864e7dcfca5b137cac5acb1d698f3fdc493e
Suchithra <Suchithra@LAPTOP-JCRI3N59>
2022-12-10 10:46:55 +0530

    First commit

---
+++ inherit/src/inherit/inheritance.java
@@ -0,0 +1,15 @@
+package inherit;
+
+public class inheritance {
+	static int  add(int a,int b)
+	{
+		return a+b;
+	}
+	
+
+	public static void main(String[] args) {
+		inheritance obj =new inheritance();
+		System.out.println(add(1,2));			
+	}
+
+}
---
+++ inherit/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Suchithra
+ *
+ */
+module inherit {
+}
